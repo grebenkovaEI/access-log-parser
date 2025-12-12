be21b1298799976f3b496d6d458974f6486bf3c4
Ekaterina Grebenkova <baton.belogo@yandex.ru>
2025-12-12 14:14:27 +0300

    Реализован метод, суммирующий два последних знака числа

--- src/main/java/ru/course/Main.java
+++ src/main/java/ru/course/Main.java
@@ -6,9 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Введите число: ");
+        System.out.println("1. Введите число: ");
         double number = sc.nextDouble();
         System.out.println("Дробная часть числа: " + Math.round(FractionalPart.fraction(number) * 1000.0)/ 1000.0);
+
+        System.out.println("2. Введите число (2 и более знаков): ");
+        int num = sc.nextInt();
+        System.out.println("Сумма двух последних знаков числа: " + SumLastNums.sumLastNums(num));
     }
 }
 
