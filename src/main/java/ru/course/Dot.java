3741769076dcbe5edb31dc35c53dcebc37bde8bd
Ekaterina Grebenkova <baton.belogo@yandex.ru>
2026-01-09 18:19:42 +0300

    ДЗ по ООП. Объекты и классы #1. Точка координат

---
+++ src/main/java/ru/course/Dot.java
@@ -0,0 +1,12 @@
+package ru.course;
+
+public class Dot {
+    int x, y;
+    public Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public String toString() {
+        return "{" + x + ";" + y + "}";
+    }
+}
