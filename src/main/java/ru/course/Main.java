1372f8316ebee71f74917d79b5a5e898de90e7af
Ekaterina Grebenkova <baton.belogo@yandex.ru>
2026-01-11 17:53:52 +0300

    ДЗ по ООП. Объекты и классы #3. Ломаная линия

--- src/main/java/ru/course/Main.java
+++ src/main/java/ru/course/Main.java
@@ -1,5 +1,7 @@
 package ru.course;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Dot d1 = new Dot(1,3);
@@ -24,6 +26,37 @@ public class Main {
         System.out.println(l3);
         double sumLength = l1.getLength() + l2.getLength() + l3.getLength();
         System.out.println(sumLength);
+        System.out.println("--------");
+
+        Dot dot1 = new Dot(1,5);
+        Dot dot2 = new Dot(2,8);
+        Dot dot3 = new Dot(5,3);
+        Dot dot4 = new Dot(8,9);
+        Dot[] arrayDot = {dot1,dot2,dot3,dot4};
+        //Создать Ломаную, проходящую через точки {1;5}, {2;8}, {5;3}, {8,9}
+        PolyLine p1 = new PolyLine(arrayDot);
+        System.out.println(p1);
+        //Рассчитать длину Ломаной
+        double p1Length = p1.getLength();
+        System.out.println("Длина ломаной: " + p1Length);
+        //Получить у Ломаной массив Линий
+        Line[] p1Lines = p1.getLines();
+        System.out.println("Массив линий ломаной: " + Arrays.toString(p1Lines));
+        //Рассчитать длину массива Линий
+        double p1LinesLength = 0;
+        for (Line line : p1Lines) {
+            p1LinesLength += line.getLength();
+        }
+        System.out.println("Длина массива линий: " + p1LinesLength);
+        //Сравнить длину Ломаной и массива Линий: они должны совпасть
+        if (p1Length == p1LinesLength) System.out.println("Длина ломаной и длина массива ломаной совпадают");
+        //Изменить координаты Точки {2,8} таким образом, чтобы она стала иметь значение {12,8}. Если изменения отразились в данной точке,
+        // в Ломаной и в двух Линиях массива (из пункта 3), то задача решена верно
+        dot2.x = 12;
+        dot2.y = 8;
+        System.out.println("Точка {2,8} изменена на " + dot2);
+        System.out.println(p1);
+        System.out.println(Arrays.toString(p1Lines));
     }
 }
 
