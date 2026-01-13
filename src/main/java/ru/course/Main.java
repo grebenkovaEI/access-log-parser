1ce1e5053e5e52a50eeccccd72f0853352dcd264
Ekaterina Grebenkova <baton.belogo@yandex.ru>
2026-01-13 19:16:14 +0300

    ДЗ по ООП. Инкапсуляция #1. Дробь

--- src/main/java/ru/course/Main.java
+++ src/main/java/ru/course/Main.java
@@ -92,6 +92,16 @@ public class Main {
         //Настройте объекты городов так, чтобы при передаче числа 3 последним городом оказался город A
         System.out.println("Задание: Настройте объекты городов так, чтобы при передаче числа 3 последним городом оказался город A");
         System.out.println("Ответ: Настройка не требуется, т.к. объекты уже настроены таким образом.");
+        System.out.println("--------");
+
+        Fraction f1 = new Fraction(1,3);
+        System.out.println(f1);
+        Fraction f2 = new Fraction(2,5);
+        System.out.println(f2);
+        Fraction f3 = new Fraction(7,8);
+        System.out.println(f3);
+        System.out.println("Посчитать f1.sum(f2).sum(f3).minus(5), где f1 это одна треть, f2 две пятых, " +
+                "а f3 это семь восьмых. Результат: " + f1.sum(f2).sum(f3).minus(5));
     }
 }
 
