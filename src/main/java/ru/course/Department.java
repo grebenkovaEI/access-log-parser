a071113710b3ddced422907a61dacaa952b4c74e
Ekaterina Grebenkova <baton.belogo@yandex.ru>
2026-01-17 14:35:43 +0300

    ДЗ по ООП. Инкапсуляция #4. Сотрудники и отделы - задание необязательное

---
+++ src/main/java/ru/course/Department.java
@@ -0,0 +1,23 @@
+package ru.course;
+
+public class Department {
+    private final String name;
+    private Employee director;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Employee getDirector() {
+        return director;
+    }
+
+    public void setDirector(Employee director) {
+        this.director = director;
+        if (director.getDepartment() != this) director.setDepartment(this);
+    }
+}
--- src/main/java/ru/course/Main.java
+++ src/main/java/ru/course/Main.java
@@ -110,6 +110,15 @@ public class Main {
         student.addGrade(3);
         student.addGrade(5);
         System.out.println(student);
+        System.out.println("--------");
+
+        Department department = new Department("IT");
+        Employee employee1 = new Employee("Иван");
+        Employee employee2 = new Employee("Семен");
+        employee2.setDepartment(department);
+        department.setDirector(employee1);
+        System.out.println(employee1);
+        System.out.println(employee2);
     }
 }
 
