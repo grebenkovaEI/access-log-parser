1112c6365fbf23e21f4e6058ce7419e073237ad3
Ekaterina Grebenkova <baton.belogo@yandex.ru>
2026-01-24 18:53:58 +0300

    ДЗ по ООП. Полиморфизм #10. Общая площадь - задание необязательное

---
+++ src/main/java/ru/course/polymorphism/SumAllAreas/SumAllAreas.java
@@ -0,0 +1,13 @@
+package ru.course.polymorphism.SumAllAreas;
+
+import ru.course.polymorphism.Figures.Figure;
+
+public class SumAllAreas {
+    public static double sumAllAreas(Figure... figures){
+        double res = 0;
+        for (Figure figure : figures){
+            res += figure.getArea();
+        }
+        return res;
+    }
+}
