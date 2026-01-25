b075dc82513772773e8afbdd95e67b398bda7352
Ekaterina Grebenkova <baton.belogo@yandex.ru>
2026-01-25 17:34:29 +0300

    ДЗ по ООП. Пакеты #4. Простые имена

---
+++ src/main/java/ru/course/main/simpleNames/Main.java
@@ -0,0 +1,14 @@
+package ru.course.main.simpleNames;
+
+import ru.course.geometry.dot.Dot;
+
+public class Main {
+    public static void main(String[] args) {
+        Dot dot1 = new Dot(1,2);
+        awt.Dot dot2 = new awt.Dot(3,4);
+
+        System.out.println(dot1 + " " + dot2);
+
+    }
+}
+
