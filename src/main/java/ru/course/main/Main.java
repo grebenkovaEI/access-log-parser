c3738c305238dc8aca34cc0dd96921332c37ba92
Ekaterina Grebenkova <baton.belogo@yandex.ru>
2026-01-25 16:42:12 +0300

    ДЗ по ООП. Пакеты #2. Главный метод

---
+++ src/main/java/ru/course/main/Main.java
@@ -0,0 +1,12 @@
+package ru.course.main;
+
+import ru.course.geometry.Measurable.Measurable;
+import ru.course.geometry.Measurable.PrintLengths;
+import ru.course.geometry.StringLength.stringLength;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
